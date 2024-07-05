078b5ff531af36a3877f0a006cce4ca43192a477
Nwigberi Moses <70588071+TheMoeEntity@users.noreply.github.com>
2024-07-05 09:19:26 +0100

    escaped quote character

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ const NotFound = () => {
             <div className="flex flex-col justify-center items-center text-center gap-8 leading-tight text-[var(--ditco-primary)]">
                 <h1 className='text-9xl font-bold text-[var(--ditco-tertiary)]'>404</h1>
                 <h1 className='text-5xl font-bold'>Page Not Found</h1>
-                <h2 className='text-xl px-3'>The page you are looking for doesn't exist or has been moved</h2>
+                <h2 className='text-xl px-3'>The page you are looking for doesn&#39;t exist or has been moved</h2>
                 <Link href={'/'} className='bg-[#1C2435] rounded-md w-fit text-white px-5 py-5'>GO TO DASHBOARD</Link>
             </div>
         </div>
